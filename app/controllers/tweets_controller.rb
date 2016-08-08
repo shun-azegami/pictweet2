c9c5feef1097e3a71714f9e23f9b1d77f6b0a085
tech-camp <tech-camp@tech-camp-046-no-MacBook-Air.local>
2016-08-08 23:55:58 +0900

    remake tweets_controller.rb

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,26 @@
+class TweetsController < ApplicationController
+
+  before_action :move_to_index, except: :index # indexアクション以外が実行される前にhogeが実行される。
+
+  def index
+    @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
+  end
+
+  def new
+  end
+
+  def create
+    Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id) #tweet_paramsはハッシュ
+
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :text, :image)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+end
