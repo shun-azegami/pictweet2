93d7bebae52c007a932402129e49c6c939121949
tech-camp <tech-camp@tech-camp-046-no-MacBook-Air.local>
2016-08-09 01:57:07 +0900

    delete function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,16 @@
 class TweetsController < ApplicationController
 
-  before_action :move_to_index, except: :index # indexアクション以外が実行される前にhogeが実行される。
+  before_action :move_to_index, except: :index
 
   def index
-    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+    @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
   end
 
   def create
-    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id) #tweet_paramsはハッシュ
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
   def destroy
@@ -20,14 +20,12 @@ class TweetsController < ApplicationController
     end
   end
 
-
   private
   def tweet_params
-    params.permit(:text, :image)
+    params.permit(:image, :text)
   end
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
-
 end
