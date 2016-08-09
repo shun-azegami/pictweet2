9969cd09ad812ce06d0cf9e067b8118bce034f66
tech-camp <tech-camp@tech-camp-046-no-MacBook-Air.local>
2016-08-09 16:57:39 +0900

    strong parameter and redirect

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(text: params[:text], tweet_id: params[:tweet_id], user_id: current_user.id)
+    Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
+    redirect_to "/tweets/#{@comment.tweet.id}"   #コメントと結びつくツイートの詳細画面に遷移する"
+  end
+
+  private
+  def comment_params
+    params.permit(:text, :tweet_id)
   end
 end
