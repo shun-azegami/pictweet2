a9d33c81102559d0e9377f721f89c7d982966ab7
tech-camp <tech-camp@tech-camp-046-no-MacBook-Air.local>
2016-08-09 17:19:12 +0900

    edit views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def show
+    user = User.find(params[:id])
     @nickname = current_user.nickname
-    @tweets = current_user.tweets.order("id DESC").page(params[:page]).per(5)
+    @tweets = user.tweets.order("id DESC").page(params[:page]).per(5)
   end
 end
