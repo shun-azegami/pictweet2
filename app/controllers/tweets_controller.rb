0d94285f8f1b7ea2fb723e3b948e30b53ffcd276
tech-camp <tech-camp@tech-camp-046-no-MacBook-Air.local>
2016-08-09 16:12:02 +0900

    edit

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -21,7 +21,14 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweets = Tweet.find(params[:id])
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
   end
 
   private
