28eaf093011802b5fdf687502f3465139c996657
tech-camp <tech-camp@tech-camp-046-no-MacBook-Air.local>
2016-08-09 16:46:31 +0900

    nest

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :tweets
-  resources :users
+  resources :tweets do
+    resources :comments, only: [:create] #commentsのルーティングのパスの中にコメントに結びつくツイートのidの情報が含まれる
+  end
+  resources :users, only: [:show]
 end
